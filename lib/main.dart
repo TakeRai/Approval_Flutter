402c316afe84100c54044146fed81dd8c5d7d662
Rai <faid156498@gmail.com>
2022-11-04 09:11:46 +0900

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:newchat/view/HomePage.dart';
 
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       GoRoute(
         path: "/home",
         builder: (context,state){
-          return HomePage();
+          return const HomePage();
         }
         ),
       // GoRoute(
